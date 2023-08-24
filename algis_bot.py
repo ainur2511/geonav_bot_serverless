8eb90aee946a8aa6595abaa0c5b96b2f18dd5f9c
ainur2511 <60270275+ainur2511@users.noreply.github.com>
2023-08-24 16:54:14 +0300

    Add files via upload

---
+++ algis_bot.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+
+def get_message_from_user(event, context):
+    message = json.loads(event['body'])
+    print('Users request: ', message)
+
+    if (user_message := message['message'].get('text')):  # Проверим, есть ли текст в сообщении
+        check_message(message['message']['chat']['id'], user_message)  # Отвечаем
+    if (user_location := message['message'].get('location')):  # Проверим, если ли location в сообщении
+        mag_field = magnetic_field_responce(user_location['latitude'], user_location['longitude'])
+        dip_angle = mag_field['inclination']
+        total_field = mag_field['totalintensity']
+        latitude = user_location['latitude']
+        longitude = user_location['longitude']
+        send_message(message['message']['chat']['id'], f'DIP angle = {dip_angle} \nTotal field = {total_field}'
+                                                       f'\nLatitude = {latitude} \nLongtitude = {longitude}')
+
+
+def check_message(chat_id, message):
+    if message.lower() in ['hello']:
+        send_message(chat_id, 'Hello')
+    elif message == '/start':
+        send_message(chat_id, 'Hi! Please, send me your location!')
+    else:
+        send_message(chat_id, 'Invalid request! Please, send me your location!')
+
+
+def magnetic_field_responce(latitude, longitude):
+    data = requests.get('https://www.ngdc.noaa.gov/geomag-web/calculators/calculateIgrfwmm',
+                        params={'key': 'EAU2y', 'lat1': latitude, 'lon1': longitude,
+                                'model': 'IGRF', 'resultFormat': 'json'}).json()  # делаем запрос магнитных составляющих
+    print('Ответ сервера Geomag-web: ', data)
+    return data['result'][0]
+
+
+def send_message(chat_id, text):
+    URL = 'https://api.telegram.org/bot'
+    TOKEN = ''
+    requests.get(f'{URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
